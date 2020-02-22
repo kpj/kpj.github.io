6b33ccc66e2be6bb8ce00a5d88b54870964b4e01
kpj <kpjkpjkpjkpjkpjkpj@gmail.com>
2020-02-22 12:07:59 +0100

    Update publish script to be able to work with GitHub user pages

--- publish.sh
+++ publish.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
-# git checkout --orphan gh-pages
+# Note: user pages must be published from master branch
+# git checkout --orphan master
 # git reset --hard
-# git commit --allow-empty -m "Init"
-# git checkout master
+# git commit --allow-empty -m "Initial commit"
+# git checkout source
 
 rm -rf _build
-git worktree add _build gh-pages
+git worktree add _build master
 make html
 cd _build
 
@@ -15,8 +16,8 @@ cp -a html/. .
 rm -r html
 
 git add .
-git commit -m "Update gh-pages (ref-commit: $(git log '--format=format:%H' master -1))"
-git push origin gh-pages
+git commit -m "Update master (ref-commit: $(git log '--format=format:%H' source -1))"
+git push origin master
 
 cd ..
 git worktree remove _build
