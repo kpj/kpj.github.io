9fd62d2d47ea1073334425d2af6fc1409275150d
kpj <kim.philipp.jablonski@gmail.com>
2021-04-17 09:01:43 +0200

    Refactor notebook execution

--- execute_all_notebooks.sh
+++ execute_all_notebooks.sh
@@ -2,6 +2,20 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+exec_notebook () {
+    nb_path="$1"
+    fname="$(basename "$nb_path")"
+
+    (
+        cd "$(dirname "$nb_path")"
+        pwd
+
+        jupyfmt -S "$fname"
+        papermill "$fname" "$fname"
+        nbstripout --keep-count --keep-output --extra-keys "cell.metadata.papermill metadata.papermill" "$fname"
+    )
+}
+
 find "./source" -name "*.ipynb" -print0 |
     while IFS= read -r -d '' line; do
         if [[ -z "$(git ls-files "$line")" ]]; then
@@ -9,10 +23,5 @@ find "./source" -name "*.ipynb" -print0 |
             continue
         fi
 
-        (
-            cd "$(dirname "$line")"
-            pwd
-            papermill "$(basename "$line")" "$(basename "$line")"
-            nbstripout --keep-count --keep-output --extra-keys "cell.metadata.papermill metadata.papermill" "$(basename "$line")"
-        )
+        exec_notebook "$line"
     done
